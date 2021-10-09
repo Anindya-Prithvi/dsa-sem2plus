7656c2a2f2acec6613a20d4f6433bec290c5dfeb
Anindya <anindya20024@iiitd.ac.in>
2021-10-10 01:46:16 +0530

    minor bugs left

--- foobar/doomsday.java
+++ foobar/doomsday.java
@@ -2,112 +2,178 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 class Solution{
-	static class Fraction{
-		long x;
-		long y;
-		public Fraction(int x, int y){
-			this.x = x;
-			this.y = y;
-		}
-		public long gcd(long a, long b){
-   			if (b==0) return a;
-   			return gcd(b,a%b);
-		}
-		public void simplify(){
-			long g = gcd(x,y);
-			if (g==1) return;
-			x = x/g;
-			y = y/g;
-		}
-		public void add(Fraction another){
-			long num = this.x*another.y + another.x*this.y;
-			long den = this.y*another.y;
-			x = num;
-			y = den;
-			simplify();
-		}
-		public void negate(){
-			this.x = -x;
-		}
-		public Fraction plusone(){
-			this.x = this.x+this.y;
-			simplify();
-			return this;
-		}
-	}
+    static class Fraction{
+        long x;
+        long y;
+        public Fraction(int x, int y){
+            this.x = x;
+            this.y = y;
+            simplify();
+        }
+        public long gcd(long a, long b){
+               if (b==0) return a;
+               return gcd(b,a%b);
+        }
+        public long abs(long x){
+            return (x<0)?-x:x;
+        }
+        public void simplify(){
+            long g = gcd(abs(x),abs(y));
+            if (g==1) return;
+            x = x/g;
+            y = y/g;
+        }
+        public void add(Fraction another){
+            long num = this.x*another.y + another.x*this.y;
+            long den = this.y*another.y;
+            this.x = num;
+            this.y = den;
+            simplify();
+        }
+        public void negate(){
+            this.x = -x;
+        }
+        public Fraction plusone(){
+            this.x = this.x+this.y;
+            simplify();
+            return this;
+        }
+        public void div(Fraction f){//this/f
+            this.x = f.y*this.x;
+            this.y = f.x*this.y;
+            simplify();
+        }
+        public void sub(Fraction another){
+            another.x = -another.x;
+            add(another);
+        }
+        public Fraction mul(Fraction another){
+            this.x = this.x*another.x;
+            this.y = this.y*another.y;
+            simplify();
+            return this;
+        }
+    }
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter rows:\t");
         int r = sc.nextInt();
-        System.out.println("Enter columns:\t");
-        int c = sc.nextInt();
-        int[][] put = new int[r][c];
+        int[][] put = new int[r][r];
         for(int j=0; j<r; j++){
-        	for(int i=0; i<c; i++){
-        		put[j][i] = sc.nextInt();
-        	}
+            for(int i=0; i<r; i++){
+                put[j][i] = sc.nextInt();
+            }
         }
         int[] arr = solution(put);
         for(int i:arr){
-        	System.out.print(i+" ");
+            System.out.print(i+" ");
         }
-		sc.close();
+        sc.close();
     }
 
     public static int[] solution(int[][] m) {
-    	int r = m.length;
-    	int sum = 0;
-    	ArrayList<Integer> qindx = new ArrayList<Integer>();
-    	ArrayList<Integer> nindx = new ArrayList<Integer>();
-    	Fraction[][] opmat = new Fraction[r][r];
-    	for(int i = 0; i<r; i++){
-    		for(int j=0; j<r; j++){
-    			sum+=m[i][j];
-    		}
-    		if(sum!=0){
-    			qindx.add(i); //add transition row name
-    		}
-    		else{
-    			nindx.add(i); //add terminal row name
-    		}
-    		if(sum==0){sum=1;}
-    		for(int j=0; j<r; j++){
-    			opmat[i][j] = new Fraction(m[i][j], sum);
-    		}
-    		sum=0;
-    	}
-		System.out.println();
-    	Fraction[][] topmat = transform(opmat, qindx, nindx);
-		for(int i=0; i<qindx.size(); i++){
-			for(int j=0; j<r; j++){
-				System.out.print(topmat[i][j].x+"\t");
-			}
-			System.out.println();
-		}
-		//handle state 0 as terminal
-		if(qindx.size()==0){
-			int[] initer = new int[r+1];
-			initer[0] = 1;
-			initer[initer.length-1] = 1;
-			return initer;
-		}
-		System.out.println();
-    	return m[0];//no dont lol
+        int r = m.length;
+        int sum = 0;
+        ArrayList<Integer> qindx = new ArrayList<Integer>();
+        ArrayList<Integer> nindx = new ArrayList<Integer>();
+        Fraction[][] opmat = new Fraction[r][r];
+        for(int i = 0; i<r; i++){
+            for(int j=0; j<r; j++){
+                sum+=m[i][j];
+            }
+            if(sum!=0){
+                qindx.add(i); //add transition row name
+            }
+            else{
+                nindx.add(i); //add terminal row name
+            }
+            if(sum==0){sum=1;}
+            for(int j=0; j<r; j++){
+                opmat[i][j] = new Fraction(m[i][j], sum);
+            }
+            sum=0;
+        }
+        System.out.println();
+        Fraction[][] topmat = transform(opmat, qindx, nindx);
+        for(int i=0; i<qindx.size(); i++){
+            for(int j=0; j<r; j++){
+                System.out.print(topmat[i][j].x+"\t");
+            }
+            System.out.println();
+        }
+        //handle state 0 as terminal
+        if(qindx.size()==0){
+            int[] initer = new int[r+1];
+            initer[0] = 1;
+            initer[initer.length-1] = 1;
+            return initer;
+        }
+        Fraction[] sol = find_sol(topmat);
+        System.out.println(sol);
+        return m[0];//no dont lol
     }
 
+    public static Fraction[] find_sol(Fraction[][] qr){
+        //qr[0][0] can never be zero
+        Fraction normalize = qr[0][0];
+        for(int i=0; i<qr[0].length; i++){
+            qr[0][i].div(normalize);
+        }
+        //qr[0][0] is now 1/1
+        for(int i=1; i<qr.length;i++){
+            zerofy(qr[i], qr[0], 0);
+        }
+        //first column is [1,0,0,0..] now
+        for(int col=1; col<qr.length; col++){
+            if(qr[0][col].x==0) continue;
+            int m = 0;
+            for(int row=1; row<qr.length; row++){
+                ++m;
+                if(qr[row][col].x!=0) break;
+            }
+            normalize = qr[m][col];
+            for(int i=0; i<qr[0].length; i++){
+                qr[m][i].div(normalize);
+            }
+            zerofy(qr[0], qr[m], col);
+        }
+
+        Fraction[] tr = new Fraction[qr[0].length-qr.length];
+        for(int i=0;i<qr[0].length-qr.length; i++){
+            tr[i] = qr[0][qr.length-1+i];
+        }
+
+        return tr;
+    }
+
+    public static void zerofy(Fraction[] tozero, Fraction[] ref, int c){
+        //r = r-r[0]*ref
+        for(int i=0; i<tozero.length; i++){
+            tozero[i].sub(tozero[c].mul(ref[i]));
+        }
+    }
+
+    // public static void rowswap(Fraction[][] qr, int i, int j){
+    // 	for(int k=0; k<qr[0].length;k++){
+    // 		Fraction temp = qr[i][k];
+    // 		qr[i][k] = qr[j][k];
+    // 		qr[j][k] = temp;
+    // 	}
+    // }
+
     public static Fraction[][] transform(Fraction[][] given, ArrayList<Integer> qidx, ArrayList<Integer> nidx){
-    	ArrayList<Integer> corres = new ArrayList<Integer>();
-    	for(int i:qidx) corres.add(i);
-    	for(int i:nidx) corres.add(i); //now corres has new ordering
-		int mtr_dim = corres.size();
-    	Fraction[][] topmat = new Fraction[qidx.size()][mtr_dim];
-		for(int i=0; i<qidx.size(); i++){
-			for(int j=0; j<mtr_dim; j++){
-				topmat[i][j]=given[corres.get(i)][corres.get(j)];
-				if((i<qidx.size())&&(j<qidx.size())) topmat[i][j].negate();
-				if(i==j) topmat[i][j].plusone();
-			}
-		}
-    	return topmat;
+        ArrayList<Integer> corres = new ArrayList<Integer>();
+        for(int i:qidx) corres.add(i);
+        for(int i:nidx) corres.add(i); //now corres has new ordering
+        int mtr_dim = corres.size();
+        Fraction[][] topmat = new Fraction[qidx.size()][mtr_dim];
+        for(int i=0; i<qidx.size(); i++){
+            for(int j=0; j<mtr_dim; j++){
+                topmat[i][j]=given[corres.get(i)][corres.get(j)];
+                if((i<qidx.size())&&(j<qidx.size())) topmat[i][j].negate();
+                if(i==j) topmat[i][j].plusone();
+            }
+        }
+        return topmat;
     }
 }
